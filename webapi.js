1a1b76943da7d4fb2e7260024f1c5ca85a5d495d
Fozi3060 <fozanjavaid3060@outlook.com>
2022-09-28 07:20:50 -0800

    updated

--- webapi.js
+++ webapi.js
@@ -9,52 +9,64 @@ form.addEventListener("submit", async function (e) {
 
   const div2 = document.createElement("div");
   const h1surah = document.createElement("h1");
-  h1surah.classList.add("surahh1")
+  h1surah.classList.add("surahh1");
   div2.classList.add("surahdiv");
   document.body.append(div2);
- div2.append(h1surah)
+  div2.append(h1surah);
 
   const searchTerm = form.elements.query.value;
   const q = searchTerm;
   const config = { params: { q: searchTerm } };
-  const audio = await axios.get(`http://api.alquran.cloud/v1/quran/ar.alafasy`);
+  const audio = await axios.get(
+    `http://api.alquran.cloud/v1/quran/ar.alafasy${q}`
+  );
 
-  // const res = await axios.get(`http://api.alquran.cloud/v1/surah/${q}`);
+console.log(audio)
 
-  const array =audio.data.data.surahs
-  console.log(array)
-let search ;
-  array.forEach(element => (
-  console.log(element)
-  
-  ));  
- 
-// console.log(array)
-// console.log(element)
+  const array = audio.data.data.surahs;
 
 
-  // h1surah.innerText=res.data.data.englishName
-  // // const ayahs = res.data.data.ayahs;
-  // const ayah = res.data.data.ayahs
-  // for (ayah of ayahs) {
-  //   const p = document.createElement("p");
-  //   p.innerText = ayah.text;
-  //   div2.append(p);
+  let SearchedName = [];
+
+  array.forEach((element) => {
+    SearchedName.push(element.englishName);
+  });
+
+ function searchStringInArray(q, SearchedName) {
+    for (var i = 0; i < SearchedName.length; i++) {
+      if (SearchedName[i].match(q)) return i;
+    }
     
-  // }
-// console.log(res.data);
-  // console.log(res.data.data.ayahs);
+  }
+  const j=searchStringInArray(q, SearchedName)
+  searchStringInArray(q, SearchedName)
 
-  //   makeImages(res.data);
+
+  const SubmitHandler = (j) => {
+    // let index = searchStringInArray(q, SearchedName)
+    array[j].ayahs.forEach((element) => {
+      console.log(element.text);
+      console.log(element.audio);
+    });
+  };
+  SubmitHandler(j);
+
   form.elements.query.value = "";
 });
 
-// const makeImages = (shows) => {
-//   for (let result of shows) {
-//     const img = document.createElement("IMG");
-//     if (result.show.image) {
-//       img.src = result.show.image.medium;
-//       document.body.append(img);
-//     }
-//   }
-// };
+// console.log(array)
+// console.log(element)
+
+// h1surah.innerText=res.data.data.englishName
+// // const ayahs = res.data.data.ayahs;
+// const ayah = res.data.data.ayahs
+// for (ayah of ayahs) {
+//   const p = document.createElement("p");
+//   p.innerText = ayah.text;
+//   div2.append(p);
+
+// }
+// console.log(res.data);
+// console.log(res.data.data.ayahs);
+
+//   makeImages(res.data);
